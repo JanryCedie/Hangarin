392437ca6a5df8e48c045ace43c444be23a284fc
Janry Cedie <you@example.com>
2026-04-07 00:40:26 +0800

    Final Commit na talaga - Premium Achievement Horizon Logo

--- hangarinenv/Hangarin/projectsite/studentorg/context_processors.py
+++
@@ -1,9 +0,0 @@
-from studentorg.models import Organization, Student, College, Program
-
-def sidebar_counts(request):
-    return {
-        'org_count': Organization.objects.count(),
-        'student_count': Student.objects.count(),
-        'college_count': College.objects.count(),
-        'program_count': Program.objects.count(),
-    }
